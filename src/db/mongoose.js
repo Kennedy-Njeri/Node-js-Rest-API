a1df3657806301e563966636a378ad99fd55c5de
Kennedy Njeri <mistakenz123@gmail.com>
2020-03-02 20:20:47 +0300

    create Task model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -27,17 +27,7 @@ mongoose.connect('mongodb://127.0.0.1:27018/task-manager-api', {
 // })
 
 
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-})
+
 
 // const task = new Task({
 //     description: 'Learn Node js',
