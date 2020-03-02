454f18ae5b304f506020ac74b18b81192435f924
Kennedy Njeri <mistakenz123@gmail.com>
2020-03-02 11:24:31 +0300

    using validators

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,11 +6,54 @@ mongoose.connect('mongodb://127.0.0.1:27018/task-manager-api', {
 })
 
 
+
+// validators- built in https://mongoosejs.com/docs/validation.html
+// npm validator
+
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true
     },
     age: {
-        type: Number
+        type: Number,
+        validate(value) {
+            if (value < 0) {
+                throw new Error("Age must be a positive number")
+            }
+        }
     }
 })
+
+
+const me = new User({
+    name: 'Vicky',
+    age: -1
+})
+
+me.save().then(() => {
+    console.log(me)
+}).catch((error) => {
+    console.log("Error", error)
+})
+
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
+})
+
+// const task = new Task({
+//     description: 'Learn Node js',
+//     completed: false
+// })
+//
+// task.save().then(() => {
+//     console.log(task)
+// }).catch(error => {
+//     console.log(error)
+// })
