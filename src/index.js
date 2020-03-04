4a2b40086bee5a1dc94065288024fe5800bb4ec1
Kennedy Njeri <mistakenz123@gmail.com>
2020-03-04 20:35:03 +0300

    async and await for task model

--- src/index.js
+++ src/index.js
@@ -76,36 +76,62 @@ app.get('/users/:id', async (req, res) => {
 
 
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
 
-    task.save().then(() => {
-        res.status(201).send(task)
-    }).catch(error => {
-        res.status(400).send(error)
-    })
+    try {
+        const tasks = await Task.save()
+        res.status(201).send(tasks)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+
+    // task.save().then(() => {
+    //     res.status(201).send(task)
+    // }).catch(error => {
+    //     res.status(400).send(error)
+    // })
 })
 
-app.get('/tasks', (req, res) => {
-    Task.find({}).then((tasks) => {
+app.get('/tasks', async (req, res) => {
+
+    try {
+        const tasks = await Task.find({})
         res.send(tasks)
-    }).catch(error => {
-        res.status(500).send()
-    })
+    } catch (e) {
+        res.status(500).send(e)
+    }
+
+    // Task.find({}).then((tasks) => {
+    //     res.send(tasks)
+    // }).catch(error => {
+    //     res.status(500).send()
+    // })
 })
 
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
     const _id = req.params.id
 
-    Task.findById(_id).then((task) => {
+    try {
+        const task = await Task.findById(_id)
         if (!task) {
-           return res.status(404).send()
+            return res.status(404).send()
         }
 
         res.send(task)
-    }).catch((error) => {
+    } catch (e) {
         res.status(500).send()
-    })
+    }
+
+    // Task.findById(_id).then((task) => {
+    //     if (!task) {
+    //        return res.status(404).send()
+    //     }
+    //
+    //     res.send(task)
+    // }).catch((error) => {
+    //     res.status(500).send()
+    // })
 })
 
 
