2ff3a472285d86d2c2f8ece46d3ab730ac0fec4e
Kennedy Njeri <mistakenz123@gmail.com>
2020-03-04 19:58:28 +0300

    async and await

--- playground/promise-chaining2.js
+++ playground/promise-chaining2.js
@@ -2,13 +2,25 @@ require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
 
-Task.findByIdAndRemove('5e5cbef4adeb751af1e31599').then((tasks) => {
-    console.log(tasks)
-    return Task.countDocuments({ completed: false })
-}).then((result) => {
+// Task.findByIdAndRemove('5e5cbef4adeb751af1e31599').then((tasks) => {
+//     console.log(tasks)
+//     return Task.countDocuments({ completed: false })
+// }).then((result) => {
+//     console.log(result)
+// }).catch(error => {
+//     console.log(error)
+// })
+
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndRemove(id)
+    const count = await Task.countDocuments({completed: false})
+    return count
+}
+
+
+deleteTaskAndCount('5e5d3fd44994641f331a32a2').then((result) => {
     console.log(result)
 }).catch(error => {
     console.log(error)
 })
-
-
--- playground/promises-chaining.js
+++ playground/promises-chaining.js
@@ -4,11 +4,24 @@ const User = require('../src/models/user')
 
 // 5e5cc704adfa861b69f0d79f https://mongoosejs.com/docs/queries.html https://mongoosejs.com/docs/api/model.html
 
-User.findByIdAndUpdate('5e5cfe3809dc271db4210ced', { age: 1 }).then((user) => {
-    console.log(user)
+// User.findByIdAndUpdate('5e5cfe3809dc271db4210ced', { age: 1 }).then((user) => {
+//     console.log(user)
+//
+//     return User.countDocuments({ age: 1 })
+// }).then((result) => {
+//     console.log(result)
+// }).catch(error => {
+//     console.log(error)
+// })
 
-    return User.countDocuments({ age: 1 })
-}).then((result) => {
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age: age})
+    const count = await User.countDocuments({age: 2})
+    return count
+}
+
+updateAgeAndCount('5e5cc704adfa861b69f0d79f', 2).then((result) => {
     console.log(result)
 }).catch(error => {
     console.log(error)
