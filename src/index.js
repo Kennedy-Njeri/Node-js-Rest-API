a1125124db42b380afe7d49eeca979252cc59545
Kennedy Njeri <mistakenz123@gmail.com>
2020-03-05 18:11:24 +0300

    working on delete

--- src/index.js
+++ src/index.js
@@ -101,6 +101,19 @@ app.patch('/users/:id', async (req, res) => {
 })
 
 
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (e) {
+        res.status(500).send()
+    }
+
+})
 
 
 app.post('/tasks', async (req, res) => {
@@ -161,6 +174,45 @@ app.get('/tasks/:id', async (req, res) => {
     // })
 })
 
+app.patch('/tasks/:id', async (req, res) => {
+    // convert an object to an array of its properties
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update)
+    })
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!'})
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if (!task) {
+            res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(500).send()
+    }
+
+})
 
 
 
