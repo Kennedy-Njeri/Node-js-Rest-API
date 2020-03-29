c9aeedd8d7c434611fb77862487768ee58900417
Kennedy Njeri <mistakenz123@gmail.com>
2020-03-29 16:29:07 +0300

    added sorting data functionality

--- src/routers/task.js
+++ src/routers/task.js
@@ -31,12 +31,20 @@ router.post('/tasks', auth, async (req, res) => {
 
 // GET /tasks?completed=true
 // GET /tasks?limit=1
+// GET /tasks?sortBy=createdAt:desc
 router.get('/tasks', auth, async (req, res) => {
     const match = {}
+    const sort = {}
 
     // filter data
     if (req.query.completed) {
         match.completed = req.query.completed === 'true'
+    }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        // grabbing the value in the first array
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
     }
 
     try {
@@ -47,7 +55,9 @@ router.get('/tasks', auth, async (req, res) => {
             match: match,
             options: {
                 limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip)
+                skip: parseInt(req.query.skip),
+                // short hand syntax
+                sort
             }
         }).execPopulate()
         res.send(req.user.tasks)
