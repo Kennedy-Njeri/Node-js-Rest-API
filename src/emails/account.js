afeb26c825929025bf2ddcf63ac6b388f3628ef0
Kennedy Njeri <mistakenz123@gmail.com>
2020-07-05 19:55:04 +0300

    sendgrid config

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,1 +1,19 @@
-const sendgridAPIKey = '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'
+const sgMail = require('@sendgrid/mail')
+
+const sendgridAPIKey = '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'
+
+sgMail.setApiKey(sendgridAPIKey)
+
+const msg = {
+    to: 'mistakenz@ymail.com',
+    from: 'mistakenz@ymail.com',
+    subject: 'Sending with Twilio SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+}
+
+sgMail.send(msg).then(() => {
+    console.log("Message Sent")
+}).catch((error) => {
+    console.log(error.response.body)
+})
