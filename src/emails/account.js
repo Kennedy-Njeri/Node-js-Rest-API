5c8ce9f3a8cdd91fd4cca049e53c66dc4eafce10
Kennedy Njeri <mistakenz123@gmail.com>
2020-07-06 23:50:11 +0300

    sendgrid config

--- src/emails/account.js
+++ src/emails/account.js
@@ -12,8 +12,3 @@ const msg = {
     html: '<strong>and easy to do anywhere, even with Node.js</strong>',
 }
 
-sgMail.send(msg).then(() => {
-    console.log("Message Sent")
-}).catch((error) => {
-    console.log(error.response.body)
-})
