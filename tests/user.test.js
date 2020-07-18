6dee9e1aff051dbe07c9841cae82305978418e78
Kennedy Njeri <mistakenz123@gmail.com>
2020-07-18 20:45:54 +0300

    test delete account user

--- tests/user.test.js
+++ tests/user.test.js
@@ -65,3 +65,20 @@ test('should not get profile for unauthenticated', async () => {
         .send()
         .expect(401)
 })
+
+
+test('should delete account for user', async () => {
+    await request(app)
+        .delete('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+})
+
+
+test('should not delete account for unauthenticated', async () => {
+    await request(app)
+        .delete('/users/me')
+        .send()
+        .expect(401)
+})
